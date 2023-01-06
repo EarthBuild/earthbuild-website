dfc16e8af05596e2fe4b9614e861c993d37fc898
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-01-06 19:22:16 +0100

    Added prettier code style

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Website for the EarthBuild Server
+
 Built with SvelteKit
 
 # How to Setup
+
 Clone Repository with `git clone https://github.com/EarthBuild/earthbuild-website.git`. <br /> Then do `npm install` in the cloned folder to install the dependencies. To start the dev server and open it in browser run `npm run dev -- --open`.
+
 # How to build
+
 Once you did all your changes do `npm run build` to build your web app. For your build environment you may have to install an adapter.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,48 @@
 <script>
-  import { webVitals } from '../vitals';
-  import { page } from '$app/stores';
-  import { inject } from '@vercel/analytics';
+	import { webVitals } from '../vitals';
+	import { page } from '$app/stores';
+	import { inject } from '@vercel/analytics';
 
-  // Load the stylesheet
-  import '../app.css';
+	// Load the stylesheet
+	import '../app.css';
 
-  // Inject vercel analytics
-  inject();
+	// Inject vercel analytics
+	inject();
 
-  // More vercel analytics
-  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+	// More vercel analytics
+	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
 
-  $: if (analyticsId) {
-    webVitals({
-      path: $page.url.pathname,
-      params: $page.params,
-      analyticsId
-    })
-  }
+	$: if (analyticsId) {
+		webVitals({
+			path: $page.url.pathname,
+			params: $page.params,
+			analyticsId
+		});
+	}
 
- function copyLink() {
-   navigator.clipboard.writeText('earthbuild.de');
- }
+	function copyLink() {
+		navigator.clipboard.writeText('earthbuild.de');
+	}
 </script>
 
 <div>
- <div class="card_left" on:click={copyLink}>
-  <div style="font-size: 24px;font-weight:700;left:20px;position:relative;">EARTHBUILD.DE</div>
-  <div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:60px;left:19px;">Klicke zum kopieren</div>
- </div>
- <div class="logo">
-  <a href="https://earthbuild.de/">
-   <img alt="Earthbuild Logo" src="/favicon.png">
-  </a>
- </div>
- <a href="https://discord.gg/fmxNNE3xnH">
-  <div class="card_right">
-    <div style="font-size: 24px;font-weight:700;left:20px;position:relative;">DISCORD</div>
-  <div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:60px;left:19px;">Klicke zum joinen</div>
-  </div>
- </a>
+	<div class="card_left" on:click={copyLink}>
+		<div style="font-size: 24px;font-weight:700;left:20px;position:relative;">EARTHBUILD.DE</div>
+		<div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:60px;left:19px;">
+			Klicke zum kopieren
+		</div>
+	</div>
+	<div class="logo">
+		<a href="https://earthbuild.de/">
+			<img alt="Earthbuild Logo" src="/favicon.png" />
+		</a>
+	</div>
+	<a href="https://discord.gg/fmxNNE3xnH">
+		<div class="card_right">
+			<div style="font-size: 24px;font-weight:700;left:20px;position:relative;">DISCORD</div>
+			<div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:60px;left:19px;">
+				Klicke zum joinen
+			</div>
+		</div>
+	</a>
 </div>

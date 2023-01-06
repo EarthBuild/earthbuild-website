6a829ac30de02703bed6dd77a56ccc2cf0c89caf
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-01-06 16:31:13 +0100

    trying something

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,20 @@
 <script>
   import '../app.css';
 
+  const isMobile = window.matchMedia('(max-width: 600px)').matches;
+
  function copyLink() {
    navigator.clipboard.writeText('earthbuild.de');
  }
 </script>
 
 <div>
+ {#if !isMobile}
  <div class="card_left" on:click={copyLink}>
   <div style="font-size: 24px;font-weight:700;left:20px;position:relative;">EARTHBUILD.DE</div>
   <div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:60px;left:19px;">Klicke zum kopieren</div>
  </div>
+ {/if}
  <div class="logo">
   <a href="https://earthbuild.de/">
    <img alt="Earthbuild Logo" src="https://cdn.discordapp.com/attachments/812383975611432960/1060555560916893837/earthbuild-removebg-preview.png">

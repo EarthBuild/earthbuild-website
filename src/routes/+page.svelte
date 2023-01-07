ae1eb642f95b8fef3b45e6bdbe99102b8cb313ba
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-01-07 14:58:39 +0100

    test it out

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,36 +21,37 @@
 	}	
 </script>
 
+
 <div>
-	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	{#if !isMobile}
-	 <div class="card_left" on:click={copyLink}>
-			<div style="font-size: 24px;font-weight:700;left:7%;position:relative;">EARTHBUILD.DE</div>
-			<div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:63%;left:7%;">
-			Klicke zum kopieren
-		</div>
-	</div>
-	{/if}
-	<div class="logo">
-		<a href="https://earthbuild.de/">
-			<img alt="Earthbuild Logo" src="/logo.webp" />
-		</a>
-	</div>
-	<a href="https://discord.gg/fmxNNE3xnH">
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		{#if !isMobile}
-		<div class="card_right">
-			<div style="font-size: 24px;font-weight:700;left:7%;position:relative;">DISCORD</div>
-			<div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:63%;left:7%;">
-				Klicke zum joinen
-			</div>
-		</div>
-		{:else}
-		<div class="card_right" style="top: 45%; right: 12%;">
-			<div style="font-size: 24px;font-weight:700;left:7%;position:relative;">DISCORD</div>
-			<div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:63%;left:7%;">
-				Klicke zum joinen
+			<div class="card_left" on:click={copyLink}>
+				<div style="font-size: 24px;font-weight:700;left:7%;position:relative;">EARTHBUILD.DE</div>
+				<div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:63%;left:7%;">
+				Klicke zum kopieren
 			</div>
 		</div>
 		{/if}
-	</a>
+		<div class="logo">
+			<a href="https://earthbuild.de/">
+				<img alt="Earthbuild Logo" src="/logo.webp" />
+			</a>
+		</div>
+		<a href="https://discord.gg/fmxNNE3xnH">
+			{#if !isMobile}
+			<div class="card_right">
+				<div style="font-size: 24px;font-weight:700;left:7%;position:relative;">DISCORD</div>
+				<div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:63%;left:7%;">
+					Klicke zum joinen
+				</div>
+			</div>
+			{:else}
+			<div class="card_right" style="top: 45%; right: 12%;">
+				<div style="font-size: 24px;font-weight:700;left:7%;position:relative;">DISCORD</div>
+				<div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:63%;left:7%;">
+					Klicke zum joinen
+				</div>
+			</div>
+			{/if}
+		</a>
 </div>

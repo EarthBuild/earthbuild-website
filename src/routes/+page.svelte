faa782ffd38372a446839a521d95b9c6d0d3e6a6
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-01-07 15:02:51 +0100

    Updated package.json

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,11 +6,18 @@
 
 	if (browser) {
 		function detectMobile() {
-   const userAgent = navigator.userAgent;
-   isMobile = userAgent.match(/Android/i) || userAgent.match(/webOS/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPad/i) || userAgent.match(/iPod/i) || userAgent.match(/BlackBerry/i) || userAgent.match(/Windows Phone/i);
+			const userAgent = navigator.userAgent;
+			isMobile =
+				userAgent.match(/Android/i) ||
+				userAgent.match(/webOS/i) ||
+				userAgent.match(/iPhone/i) ||
+				userAgent.match(/iPad/i) ||
+				userAgent.match(/iPod/i) ||
+				userAgent.match(/BlackBerry/i) ||
+				userAgent.match(/Windows Phone/i);
 		}
 
-		detectMobile()
+		detectMobile();
 	}
 
 	// Load the stylesheet
@@ -18,40 +25,39 @@
 
 	function copyLink() {
 		navigator.clipboard.writeText('earthbuild.de');
-	}	
+	}
 </script>
 
-
 <div>
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		{#if !isMobile}
-			<div class="card_left" on:click={copyLink}>
-				<div style="font-size: 24px;font-weight:700;left:7%;position:relative;">EARTHBUILD.DE</div>
-				<div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:63%;left:7%;">
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	{#if !isMobile}
+		<div class="card_left" on:click={copyLink}>
+			<div style="font-size: 24px;font-weight:700;left:7%;position:relative;">EARTHBUILD.DE</div>
+			<div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:63%;left:7%;">
 				Klicke zum kopieren
 			</div>
 		</div>
-		{/if}
-		<div class="logo">
-			<a href="https://earthbuild.de/">
-				<img alt="Earthbuild Logo" src="/logo.webp" />
-			</a>
-		</div>
-		<a href="https://discord.gg/fmxNNE3xnH">
-			{#if !isMobile}
+	{/if}
+	<div class="logo">
+		<a href="https://earthbuild.de/">
+			<img alt="Earthbuild Logo" src="/logo.webp" />
+		</a>
+	</div>
+	<a href="https://discord.gg/fmxNNE3xnH">
+		{#if !isMobile}
 			<div class="card_right">
 				<div style="font-size: 24px;font-weight:700;left:7%;position:relative;">DISCORD</div>
 				<div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:63%;left:7%;">
 					Klicke zum joinen
 				</div>
 			</div>
-			{:else}
+		{:else}
 			<div class="card_right" style="top: 45%; right: 12%;">
 				<div style="font-size: 24px;font-weight:700;left:7%;position:relative;">DISCORD</div>
 				<div style="font-size: 14px;font-weight:700;color:blue;position:absolute;top:63%;left:7%;">
 					Klicke zum joinen
 				</div>
 			</div>
-			{/if}
-		</a>
+		{/if}
+	</a>
 </div>
